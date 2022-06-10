54af5e2d9f1ec9182b1e34d78133480ef5557058
Abdullah1357810 <107245808+Abdullah1357810@users.noreply.github.com>
2022-06-10 11:29:50 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,247 @@
+input.onButtonPressed(Button.A, function () {
+    soundExpression.happy.play()
+    basic.showString("I LOVE YOU")
+    basic.pause(100)
+    basic.showString("MAMA")
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(5000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.AB, function () {
+    soundExpression.happy.play()
+    basic.showString("I LOVE YOU")
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(5000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    soundExpression.happy.play()
+    basic.showString("I LOVE YOU")
+    basic.pause(100)
+    basic.showString("BABA")
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+    basic.showIcon(IconNames.Heart)
+    basic.pause(5000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
